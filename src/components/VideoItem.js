e997882ea299820706805ae1c1d90142f4362cad
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2020-02-23 19:08:23 -0500

    render thmbnail and title

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const VideoItem = (props) => {
-  return <div>videoItem</div>
+const VideoItem = ({video}) => {
+  return (
+    <div>
+    <img src={video.snippet.thumbnails.medium.url} alt="video"/>
+      {video.snippet.title}
+    </div>
+
+  )
 };
 
 export default VideoItem;
